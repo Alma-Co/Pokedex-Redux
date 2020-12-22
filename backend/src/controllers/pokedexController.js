68858356bd2d85f70341ed8ab3853fc594eeee9b
Alma-Co <edith.cantegrit@gmail.com>
2020-12-22 23:34:44 +0100

    back changes

--- backend/src/controllers/pokedexController.js
+++ backend/src/controllers/pokedexController.js
@@ -15,3 +15,26 @@ function pokedexController() {
 }
 
 module.exports = pokedexController;
+
+async function getPokedex(req, res) {
+  const pokedex = {};
+  let pokemonData = [];
+  const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
+  const pokeImgUrl = 'https://pokeapi.co/api/v2/pokemon-form';
+  try {
+    const { data } = await axios.get(pokeApiUrl);
+    const pokemonList = data.results;
+    for (let id = 1; id < 50; id++) {
+      const { data } = await axios.get(`${pokeImgUrl}/${id}`);
+      pokemonData = [...pokemonData, data.sprites.front_default];
+    }
+    res.send(pokemonData);
+  } catch (error) {
+    res.send(error);
+  }
+}
+
+// pokemonList.map(obj=> ({
+//   ...obj,
+//   const id = obj.url.split('/')[5];
+//   picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`  }));
